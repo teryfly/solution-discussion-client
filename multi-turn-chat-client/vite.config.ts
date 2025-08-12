2c84a8bad7802836dcec491ebb8acf4979c4a96a
Tery <teryfly@qq.com>
2025-08-12 21:27:18 +0800

    不按源码实时刷新

--- multi-turn-chat-client/vite.config.ts
+++ multi-turn-chat-client/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: false, // 禁用热更新（HMR）
+  },
 })
